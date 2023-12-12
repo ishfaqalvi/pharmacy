0d28f31067f81b49c448810f49aca384fa412eb0
Ishfaq Alvi <ishfaq.alvi.33@gmail.com>
2023-12-12 22:53:02 +0500

    Update prices

--- resources/views/admin/product/price/index.blade.php
+++ resources/views/admin/product/price/index.blade.php
@@ -13,7 +13,7 @@
     </div>
     <div class="d-lg-block my-lg-auto ms-lg-auto">
         <div class="d-sm-flex align-items-center mb-3 mb-lg-0 ms-lg-3">
-            <a href="{{ route('products.index') }}" class="btn btn-outline-primary btn-labeled btn-labeled-start rounded-pill me-2">
+            <a href="{{ route('products.all.index') }}" class="btn btn-outline-primary btn-labeled btn-labeled-start rounded-pill me-2">
                 <span class="btn-labeled-icon bg-primary text-white rounded-pill">
                     <i class="ph-arrow-circle-left"></i>
                 </span>
